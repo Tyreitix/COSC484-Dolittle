3dc04c1f34e114712c8a66e968ef7441afd53327
GabrielAddai <gaddai1@students.towson.edu>
2023-05-16 14:21:56 -0400

    fixed homepage routing

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -6,6 +6,8 @@ import "bootstrap/dist/css/bootstrap.css";
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
 
+//this creates a html page that acts as the first page that loads when you go to our website
 export default function Homepage() {
     return <h1>Welcome to the Home Page!</h1>;
+    
 }
