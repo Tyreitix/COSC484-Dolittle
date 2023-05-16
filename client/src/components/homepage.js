da15b7af48d7de1fe5d931d5110d6132511f45a0
GabrielAddai <gaddai1@students.towson.edu>
2023-05-16 13:19:39 -0400

    added new homepage

---
+++ client/src/components/homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ 
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+
+export default function Homepage() {
+    return <h1>Welcome to the Home Page!</h1>;
+}
